07b869e9eea562e23efd2095ed6a4025f60b5029
1ahmadsam <samiahmad360@hotmail.com>
2020-05-14 06:51:58 -0400

    finished first 2 tests

--- part 4/bloglist/utils/list_helper.js
+++ part 4/bloglist/utils/list_helper.js
@@ -1,7 +1,34 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => {
+    return accumulator + currentValue.likes;
+  };
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+const favouriteBlog = (blogs) => {
+  const reducer = (prev, currentValue) => {
+    if (prev.likes > currentValue.likes) {
+      return prev;
+    } else {
+      return currentValue;
+    }
+  };
+  return blogs.length === 0 ? blogs : blogs.reduce(reducer, 0);
+};
+
+const mostBlogs = (blogs) => {
+  return _.head(_(blogs.author).countBy().entries().maxBy(_.last));
+};
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
+  mostBlogs,
 };
