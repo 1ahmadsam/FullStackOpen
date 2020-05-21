32c3cbe2aba39701c5cbadc26292c6bede97a8f5
1ahmadsam <samiahmad360@hotmail.com>
2020-05-21 07:07:50 -0400

    added local storage implementation with login functionality

--- part 5/bloglist-frontend/src/App.js
+++ part 5/bloglist-frontend/src/App.js
@@ -1,19 +1,110 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      //noteService.setToken(user.token)
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  // const blogForm = () => {
+  //   const addBlog = () => {};
+  //   <form onSubmit={addBlog}>
+  //     <input
+  //       value={newBlog}
+  //       onChange={({ target }) => setNewBlog(target.value)}
+  //     />
+  //     <button type='submit'>save</button>
+  //   </form>;
+  // };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  );
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+
+        <h2>{errorMessage ? errorMessage : null}</h2>
+        {loginForm()}
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      {/* // Temp */}
+      <p>
+        {user.name} logged in{' '}
+        <button
+          onClick={() => {
+            setUser(null);
+            window.localStorage.removeItem('loggedBlogappUser');
+          }}
+        >
+          logout
+        </button>
+      </p>
+      <h2>{errorMessage ? errorMessage : null}</h2>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
