797b4295fa78155d16cdb117f666d7b10698b647
1ahmadsam <samiahmad360@hotmail.com>
2020-06-12 04:14:25 -0400

    added first part

--- part 7/routed-anecdotes/src/App.js
+++ part 7/routed-anecdotes/src/App.js
@@ -8,6 +8,7 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+import { useField } from './hooks';
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -101,24 +102,32 @@ const Footer = () => (
 
 const CreateNew = (props) => {
   const history = useHistory();
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  // const [content, setContent] = useState('');
+  // const [author, setAuthor] = useState('');
+  // const [info, setInfo] = useState('');
+  const content = useField('text');
+  const author = useField('text');
+  const info = useField('text');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     history.push('/');
     //notification
-    props.setNotification(`a new anecdote ${content} created!`);
+    props.setNotification(`a new anecdote ${content.value} created!`);
     setTimeout(() => {
       props.setNotification('');
     }, 10 * 1000);
+  };
+  const clearAll = () => {
+    content.clearField;
+    author.clearField;
+    info.clearField;
   };
 
   return (
@@ -127,29 +136,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type='button' onClick={clearField}>
+          reset
+        </button>
       </form>
     </div>
   );
