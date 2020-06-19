d5ed2d97467b952e370924ba4f68ff83d75598b5
1ahmadsam <samiahmad360@hotmail.com>
2020-06-19 04:48:01 -0400

    readding

--- part 6/redux-anecdotes/src/components/AnecdoteList.js
+++ part 6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,42 +1,48 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { voteFor } from '../reducers/anecdoteReducer';
 import { createNotification } from '../reducers/notificationReducer';
 import Notification from './Notification';
 import Filter from './Filter';
 
 const AnecdoteList = (props) => {
-  const anecdotes = useSelector((state) => state.anecdote);
-  const new_filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
+  // const anecdotes = useSelector((state) => state.anecdote);
+  // const new_filter = useSelector((state) => state.filter);
+
   const vote = (anecdote) => {
     console.log('vote', anecdote.id);
     dispatch(voteFor(anecdote));
     dispatch(createNotification(`you voted '${anecdote.content}'`, 10));
   };
+  const anecdotesToShow = () => {
+    let anecdotes_sorted = props.anecdotes.sort(function (a, b) {
+      if (a.votes > b.votes) {
+        return -1;
+      }
+      if (a.votes < b.votes) {
+        return 1;
+      }
+      return 0;
+    });
+    console.log(props.anecdote);
+    return props.filter
+      ? anecdotes_sorted.filter((anecdote) =>
+          anecdote.content.includes(props.filter)
+        )
+      : anecdotes_sorted;
+  };
+
   // sort anecdotes by most votes
-  let anecdotes_sorted = anecdotes.sort(function (a, b) {
-    if (a.votes > b.votes) {
-      return -1;
-    }
-    if (a.votes < b.votes) {
-      return 1;
-    }
-    return 0;
-  });
+
   // filter anecdotes if filter exists
-  anecdotes_sorted = new_filter
-    ? anecdotes_sorted.filter((anecdote) =>
-        anecdote.content.includes(new_filter)
-      )
-    : anecdotes_sorted;
 
   return (
     <React.Fragment>
       <h2>Anecdotes</h2>
       <Filter />
       <Notification />
-      {anecdotes_sorted.map((anecdote) => (
+      {anecdotesToShow().map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
@@ -48,4 +54,13 @@ const AnecdoteList = (props) => {
     </React.Fragment>
   );
 };
-export default AnecdoteList;
+const mapStateToProps = (state) => {
+  // sometimes it is useful to console log from mapStateToProps
+  console.log(state);
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter,
+  };
+};
+const ConnectedAnecdotes = connect(mapStateToProps)(AnecdoteList);
+export default ConnectedAnecdotes;
