85e252d69a2ff8d64d4809de12f24d3707126d1b
1ahmadsam <samiahmad360@hotmail.com>
2020-06-19 17:17:47 -0400

    added basic queries

--- part 8/library-backend/index.js
+++ part 8/library-backend/index.js
@@ -84,9 +84,23 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -94,6 +108,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors,
   },
 };
 
