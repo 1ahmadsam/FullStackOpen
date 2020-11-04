0694af2410370b9b2164492631623651fc1c1a6b
1ahmadsam <samiahmad360@hotmail.com>
2020-11-03 21:08:35 -0500

    bmi calc is using cmd  args

--- part 9/bmiCalculator.ts
+++ part 9/bmiCalculator.ts
@@ -1,5 +1,28 @@
+interface BmiValues {
+    height:number;
+    weight:number;
+}
+
+
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+        height: Number(args[2]),
+        weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
+
 
 const calculateBmi = (height: number, weight: number ) : string => {
+    
     const BMI = weight / Math.pow((height / 100), 2)
     if (BMI < 15) {
         return 'Very severely underweight'
@@ -26,4 +49,11 @@ const calculateBmi = (height: number, weight: number ) : string => {
         return 'Obese Class III (Very severely obese)'
     }
 }
-console.log(calculateBmi(180, 74))
+
+try { 
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight))
+}
+catch (err) {
+    console.log('Error, something bad happened, message: ', err.message);
+}
